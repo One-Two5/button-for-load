84bfeb7a688092f2d9c0427a4fe75ace30bf1659
One-Two5 <pavel.twix1999@gmail.com>
2026-06-25 15:48:18 +0300

    Инициализация проекта "Кнопка загрузки"

---
+++ src/main/java/org/example/buttonforload/ButtonForLoadApplication.java
@@ -0,0 +1,13 @@
+package org.example.buttonforload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButtonForLoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButtonForLoadApplication.class, args);
+    }
+
+}
