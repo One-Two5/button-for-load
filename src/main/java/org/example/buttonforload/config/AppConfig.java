7b3909b87b8cb52f1677a601670531bbee0f1009
One-Two5 <pavel.twix1999@gmail.com>
2026-06-29 16:21:55 +0300

    обновил конфиг RestTemplate

--- src/main/java/org/example/buttonforload/config/AppConfig.java
+++ src/main/java/org/example/buttonforload/config/AppConfig.java
@@ -2,14 +2,41 @@ package org.example.buttonforload.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.net.http.HttpClient;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    public RestTemplate unsafeRestTemplate() throws Exception {
 
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return null; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+        };
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+
+        HttpClient httpClient = HttpClient.newBuilder()
+                .sslContext(sslContext)
+                .sslParameters(new javax.net.ssl.SSLParameters() {{
+                    setEndpointIdentificationAlgorithm("");
+                }})
+                .build();
+
+        var requestFactory = new JdkClientHttpRequestFactory(httpClient);
+
+        return new RestTemplate(requestFactory);
+    }
 }
