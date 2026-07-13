f142a9c71104cae35ed9adb98f0621ad2a34813b
One-Two5 <pavel.twix1999@gmail.com>
2026-07-13 17:21:50 +0300

    переработал юнит тесты

--- src/test/java/org/example/buttonforload/service/XlsxParseServiceTest.java
+++ src/test/java/org/example/buttonforload/service/XlsxParseServiceTest.java
@@ -1,138 +1,88 @@
 package org.example.buttonforload.service;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.buttonforload.dto.ResourceRowDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDate;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class XlsxParseServiceTest {
 
-    private XlsxParseService xlsxParseService;
+    private static XlsxParseService xlsxParseService;
+    private static Path filePath;
+    private static List<ResourceRowDto> result;
 
     @BeforeEach
     void setUp() {
         xlsxParseService = new XlsxParseService();
+        filePath = Paths.get("src", "test", "resources", "export.xlsx");
+        assertTrue(Files.exists(filePath), "Тестовый файл export.xlsx не найден!");
+        result = xlsxParseService.parse(filePath);
     }
 
     @Test
-    void parse_ValidXlsx_ReturnsMappedDtoList(@TempDir Path tempDir) throws IOException {
-        Path filePath = tempDir.resolve("valid_data.xlsx");
-        try (Workbook workbook = new XSSFWorkbook()) {
-            Sheet sheet = workbook.createSheet("Sheet1");
+    void shouldParseAllValidRowsFromXlsxFile() {
 
-            sheet.createRow(0);
+       assertNotNull(result, "Результат парсинга не должен быть null");
+       assertFalse(result.isEmpty(), "Сервис должен содержать строки с данными");
 
-            Row headerRow = sheet.createRow(1);
-            headerRow.createCell(0).setCellValue("№ п/п");
-            headerRow.createCell(1).setCellValue("Полное наименование");
-            headerRow.createCell(2).setCellValue("Основания");
-            headerRow.createCell(3).setCellValue("Дата включения");
-            headerRow.createCell(4).setCellValue("Дата исключения");
+       for (ResourceRowDto dto : result) {
+           assertNotNull(dto.getNumber(), "Порядковый номер должен быть заполнен для всех записей");
+           assertNotNull(dto.getFullName(), "Имя/Наименование агента не должно быть пустым");
 
-            Row firstRow = sheet.createRow(2);
-            firstRow.createCell(0).setCellValue(1);
-            firstRow.createCell(1).setCellValue("«Евразийская антимонопольная ассоциация»");
-            firstRow.createCell(2).setCellValue("Статья 32 ФЗ");
-            firstRow.createCell(3).setCellValue("27.06.2013");
-            firstRow.createCell(4).setCellValue("05.07.2023");
-
-            Row secondRow = sheet.createRow(3);
-            secondRow.createCell(0).setCellValue(2);
-            secondRow.createCell(1).setCellValue("Ассоциация ГОЛОС");
-            secondRow.createCell(2).setCellValue("Статья 32 ФЗ");
-            secondRow.createCell(3).setCellValue("05.06.2014");
-            secondRow.createCell(4).setCellValue("");
-
-            try (FileOutputStream fos = new FileOutputStream(filePath.toFile())) {
-                workbook.write(fos);
-            }
-        }
-
-        List<ResourceRowDto> result = xlsxParseService.parse(filePath);
-
-        assertNotNull(result);
-        assertEquals(2, result.size());
-
-        ResourceRowDto firstDto = result.get(0);
-        assertEquals(1, firstDto.getNumber());
-        assertEquals("«Евразийская антимонопольная ассоциация»", firstDto.getFullName());
-        assertEquals("Статья 32 ФЗ", firstDto.getInclusionGrounds());
-        assertEquals(LocalDate.of(2013, 6, 27), firstDto.getInclusionDecisionDate());
-        assertEquals(LocalDate.of(2023, 7, 5), firstDto.getExclusionDecisionDate());
-
-        ResourceRowDto secondDto = result.get(1);
-        assertEquals(2, secondDto.getNumber());
-        assertEquals("Ассоциация ГОЛОС", secondDto.getFullName());
-        assertNull(secondDto.getExclusionDecisionDate());
+           assertTrue(dto.getNumber() > 0, "Некорректный номер строки");
+       }
     }
 
     @Test
-    void  parse_RowWithTextInIndexCell_IsSkipped(@TempDir Path tempDir) throws IOException {
-        Path filePath = tempDir.resolve("invalid_row.xlsx");
-        try (Workbook workbook = new XSSFWorkbook()) {
-            Sheet sheet = workbook.createSheet("Sheet1");
+    void shouldSkipHeaderRow() {
 
-            Row headerRow = sheet.createRow(1);
-            headerRow.createCell(0).setCellValue("Это заголовок, тут нет числа");
-            headerRow.createCell(1).setCellValue("Тестовое имя");
+        boolean hasHeaderInResult = result.stream()
+                .anyMatch(dto -> dto.getFullName() != null && dto.getFullName()
+                        .contains("Полное наименование"));
 
-            Row dataRow = sheet.createRow(2);
-            dataRow.createCell(0).setCellValue(1);
-            dataRow.createCell(1).setCellValue("Корректное имя");
-
-            try (FileOutputStream fos = new FileOutputStream(filePath.toFile())) {
-                workbook.write(fos);
-            }
-        }
-
-        List<ResourceRowDto> result = xlsxParseService.parse(filePath);
-
-        assertEquals(1, result.size());
-        assertEquals("Корректное имя", result.get(0).getFullName());
+        assertFalse(hasHeaderInResult, "Строка заголовков колонок должна быть пропущена");
     }
 
     @Test
-    void parse_CorruptedOrEmptyFile_ThrowsRuntimeException(@TempDir Path tempDir) throws IOException {
-        Path filePath = tempDir.resolve("empty.xlsx");
-        try (Workbook workbook = new XSSFWorkbook()) {
-            try (FileOutputStream fos = new FileOutputStream(filePath.toFile())) {
-                workbook.write(fos);
+    void shouldSetNullForMissingOptionalFields() {
+
+        boolean nullField = false;
+
+        for (ResourceRowDto dto : result) {
+
+            if (dto.getFullName() != null) {
+                assertFalse(dto.getFullName().isBlank(),
+                        "Строковое поле не должно быть пустым, если оно не null");
+            }
+            if (dto.getInclusionGrounds() != null) {
+                assertFalse(dto.getInclusionGrounds().isBlank(),
+                        "Строковое поле не должно быть пустым, если оно не null");
+            }
+
+            if (dto.getFullName() == null
+                    || dto.getInclusionGrounds() == null
+                    || dto.getInclusionDecisionDate() == null
+                    || dto.getExclusionDecisionDate() == null) {
+                nullField = true;
             }
         }
 
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> xlsxParseService.parse(filePath));
+        assertTrue(nullField,
+                "В таблице нет пустых полей");
+    }
 
+    @Test
+    void shouldThrowExceptionWhenFileDoesNotExist() {
+
+        Path wrongFile = Paths.get("src", "test", "resources", "notExist.xlsx");
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> xlsxParseService.parse(wrongFile));
         assertTrue(exception.getMessage().contains("Не удалось прочитать XLSX файл"));
-    }
-
-    @Test
-    void parse_RowWithOnlyIndexAndNoData_IsSkipped(@TempDir Path tempDir) throws IOException {
-        Path filePath = tempDir.resolve("blank_row.xlsx");
-        try (Workbook workbook = new XSSFWorkbook()) {
-            Sheet sheet = workbook.createSheet("Sheet1");
-
-            Row dataRow = sheet.createRow(2);
-            dataRow.createCell(0).setCellValue(999);
-            dataRow.createCell(1).setCellValue("");
-            dataRow.createCell(2).setCellValue("   "); // пробелы
-
-            try (FileOutputStream fos = new FileOutputStream(filePath.toFile())) {
-                workbook.write(fos);
-            }
-        }
-
-        List<ResourceRowDto> result = xlsxParseService.parse(filePath);
-
-        assertTrue(result.isEmpty() || result.get(0).getFullName() == null);
     }
 }
