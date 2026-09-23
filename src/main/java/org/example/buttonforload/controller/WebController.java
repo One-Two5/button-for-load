abf3d4f9e42daea652a5cc6a67c0baad8d41421d
One-Two5 <pavel.twix1999@gmail.com>
2026-09-23 16:23:52 +0300

    добавил сервис загрузки выписки

--- src/main/java/org/example/buttonforload/controller/WebController.java
+++ src/main/java/org/example/buttonforload/controller/WebController.java
@@ -1,29 +1,38 @@
 package org.example.buttonforload.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.example.buttonforload.dto.ImportResultDto;
+import org.example.buttonforload.dto.InnProcessingResult;
+import org.example.buttonforload.service.EgrulDownloadService;
 import org.example.buttonforload.service.FileImportService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class WebController {
 
     private final FileImportService fileImportService;
-
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("result", null);
-        return "index";
-    }
+    private final EgrulDownloadService egrulDownloadService;
 
     @PostMapping("/import")
     public String importFile(Model model) {
         ImportResultDto resultDto = fileImportService.importFile();
         model.addAttribute("result", resultDto);
         return "index";
+    }
+
+    @PostMapping("/download")
+    public ResponseEntity<List<InnProcessingResult>> downloadExtracts(@RequestBody List<String> innList,
+                                                                      @RequestParam(defaultValue = "./downloads")
+                                                                      String outputFolder) {
+        List<InnProcessingResult> results = egrulDownloadService.downloadPdfExtracts(innList, outputFolder);
+        return ResponseEntity.ok(results);
     }
 }
